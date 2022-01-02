6cd2540f299cbaac6095b183a3157490d55501ab
Elia Robyn Lake <gh@arborelia.net>
2022-01-02 12:33:34 -0500

    fix some tweet boundaries to fit in 280 characters

--- tests/test_sillyoldbot.py
+++ tests/test_sillyoldbot.py
@@ -1,5 +1,13 @@
-from sillyoldbot import __version__
+from sillyoldbot import get_tweets, get_data
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_status():
+    status = get_data()
+    assert isinstance(status["current"], int)
+
+
+def test_tweets():
+    tweets = get_tweets()
+    assert len(tweets) > 100
+    for tweet in tweets:
+        assert len(tweet) <= 280, tweet
